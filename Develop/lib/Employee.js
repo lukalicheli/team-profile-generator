ce003f58f40254b39279e1e3421314a56c672895
Luka Licheli <lukalicheli@gmail.com>
2022-01-27 23:20:59 -0800

    Finished setting up main class constructor

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,25 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName(name) {
+        return this.name;
+    };
+
+    getId(id) {
+        return this.id;
+    };
+
+    getEmail(email) {
+        return this.email;
+    };
+
+    getRole() {
+        return "Employee";
+    };
+}
+
+module.exports = Employee;
