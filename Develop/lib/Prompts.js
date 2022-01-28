c8a443e2b000cc15c632ec5f513c457f5965bd86
Luka Licheli <lukalicheli@gmail.com>
2022-01-28 01:43:22 -0800

    Finished add employee constructor

--- Develop/lib/Prompts.js
+++ Develop/lib/Prompts.js
@@ -38,6 +38,91 @@ class Prompts {
         .then(({managerName, managerId, managerEmail, managerNumber}) => {
             const manager = new Manager(managerName, managerId, managerEmail, managerNumber);
             this.managers.push(manager);
+            this.internPrompt();
         })
     }
+    internPrompt() {
+        inquirer
+        .prompt([
+            {
+                type: "input", 
+                message: "What is the intern's name?",
+                name: "internName"
+            },
+            {
+                type: "input", 
+                message: "What is the intern's id number?",
+                name: "internId"
+            },
+            {
+                type: "input", 
+                message: "What is the intern's email address?",
+                name: "internEmail"
+            },
+            {
+                type: "input", 
+                message: "What school is the intern currently attending?",
+                name: "internNumber"
+            }
+        ])
+        .then(({internName, internId, internEmail, internNumber}) => {
+            const intern = new Intern(internName, internId, internEmail, internNumber);
+            this.interns.push(intern);
+            this.engineerPrompt();
+        })
+    }
+    engineerPrompt() {
+        inquirer
+        .prompt([
+            {
+                type: "input", 
+                message: "What is the engineer's name?",
+                name: "engineerName"
+            },
+            {
+                type: "input", 
+                message: "What is the engineer's id number?",
+                name: "engineerId"
+            },
+            {
+                type: "input", 
+                message: "What is the engineer's email address?",
+                name: "engineerEmail"
+            },
+            {
+                type: "input", 
+                message: "What is the engineer's GitHub username?",
+                name: "engineerGitHub"
+            }
+        ])
+        .then(({engineerName, engineerId, engineerEmail, engineerGitHub}) => {
+            const engineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGitHub);
+            this.engineers.push(engineer);
+            this.addEmployee();
+        })
+    }
+    addEmployee() {
+        inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Do you want to add another employee?",
+                choices: ["Intern", "Engineer", "Finish building my team"],
+                name: "buildTeam"
+            }
+        ])
+        .then(({buildTeam}) => {
+            if (buildTeam == "Intern") {
+                this.internPrompt();
+            } else if(buildTeam == "Engineer") {
+                this.engineerPrompt();
+            } else {
+                this.writeFile();
+            }
+        });
+    }
 };
+
+fs
+
+managerPrompt();
